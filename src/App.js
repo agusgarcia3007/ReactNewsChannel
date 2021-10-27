269e4153cd0a476202a8ca4258aca995428e1ecb
Agustin García dos Santos <agusgarcia3007@gmail.com>
2021-10-27 10:53:27 -0300

    everything working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
+import Form from './components/Form';
 
 const App = () => {
   return ( 
     <>
       <Header title='React News App'/>
+
+      <div className="container white">
+        <Form />
+      </div>
     </>
    );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import styles from'./Form.module.css';
+import useSelector from '../hooks/useSelector'
+
+const Form = () => {
+
+
+    
+    const categories = [
+        { value: 'general', label:'General'},
+        { value: 'business', label: 'Business'},
+        { value: 'entertainment', label: 'Entertainment'},
+        { value: 'health', label: 'Health'},
+        { value: 'science', label: 'Science'},
+        { value: 'sports', label: 'Sports'},
+        { value: 'technology', label: 'Tech'},
+    ]
+    
+    //using useSelector
+    const [category, SelectNews] = useSelector('general', categories);
+
+    return ( 
+        <div className={`${styles.searching} row`}>
+            <div className="col s12 m8 offset-m2">
+                <form
+                
+                >
+                    <h2 className={styles.heading}>Find Word's News by Category</h2>
+
+                    <SelectNews />
+
+                    <div className="input-field col s12">
+                        <input 
+                            type="submit" 
+                            value="Search" 
+                            className={`${styles.btn_block} btn-large amber darken-2`}
+                        />
+                    </div>
+                </form>
+            </div>
+        </div>
+     );
+}
+ 
+export default Form;
